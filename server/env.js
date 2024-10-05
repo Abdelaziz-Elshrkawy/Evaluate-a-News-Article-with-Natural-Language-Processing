0c25b776a7bcd5d4f2e40b418304f2f6db60dde2
abdelaziz-elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2024-10-05 16:54:48 +0300

    removing env.js

--- server/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Api_Id = exports.Api_Key = exports.port = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-_a = process.env, exports.port = _a.port, exports.Api_Key = _a.Api_Key, exports.Api_Id = _a.Api_Id;
